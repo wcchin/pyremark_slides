33056093a1346d17c875f42bac37e9ada0f4b709
wcchin <wcchin.88@gmail.com>
2025-03-27 14:14:57 +0800

    add screenshots of themes and thumbnails

---
+++ theme_demo/generate_thumbnails.sh
@@ -0,0 +1,16 @@
+convert -resize x150 -quality 50 demo-Catppuccin-Latte-title.png demo-Catppuccin-Latte-title.tb.png &
+convert -resize x150 -quality 50 demo-Catppuccin-Mocha-title.png demo-Catppuccin-Mocha-title.tb.png &
+convert -resize x150 -quality 50 demo-Catppuccin-Latte-content.png demo-Catppuccin-Latte-content.tb.png & 
+convert -resize x150 -quality 50 demo-Catppuccin-Mocha-content.png demo-Catppuccin-Mocha-content.tb.png &
+convert -resize x150 -quality 50 demo-Nord-Storm-title.png demo-Nord-Storm-title.tb.png &
+convert -resize x150 -quality 50 demo-Nord-Polar-title.png demo-Nord-Polar-title.tb.png &
+convert -resize x150 -quality 50 demo-Nord-Storm-content.png demo-Nord-Storm-content.tb.png & 
+convert -resize x150 -quality 50 demo-Nord-Polar-content.png demo-Nord-Polar-content.tb.png &
+convert -resize x150 -quality 50 demo-Rosepine-Moon-title.png demo-Rosepine-Moon-title.tb.png &
+convert -resize x150 -quality 50 demo-Rosepine-Dawn-title.png demo-Rosepine-Dawn-title.tb.png &
+convert -resize x150 -quality 50 demo-Rosepine-Moon-content.png demo-Rosepine-Moon-content.tb.png & 
+convert -resize x150 -quality 50 demo-Rosepine-Dawn-content.png demo-Rosepine-Dawn-content.tb.png &
+convert -resize x150 -quality 50 demo-Everforest-Day-title.png demo-Everforest-Day-title.tb.png &
+convert -resize x150 -quality 50 demo-Everforest-Night-title.png demo-Everforest-Night-title.tb.png &
+convert -resize x150 -quality 50 demo-Everforest-Day-content.png demo-Everforest-Day-content.tb.png & 
+convert -resize x150 -quality 50 demo-Everforest-Night-content.png demo-Everforest-Night-content.tb.png 
